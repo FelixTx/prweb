a26b8e52a70688967345257b82840cb0f1005cd2
FelixTx <felix.tournoux@gmail.com>
2016-02-24 17:08:00 +0100

    ca remarche

--- src/java/com/centrale/prweb/identify.java
+++ src/java/com/centrale/prweb/identify.java
@@ -24,32 +24,6 @@ import javax.servlet.http.HttpServletResponse;
 public class identify extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet identify</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet identify at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    /**
      * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
@@ -59,6 +33,7 @@ public class identify extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+            System.out.println("doget");
             jspcalling("index.jsp", request, response);
     }
 
@@ -73,8 +48,9 @@ public class identify extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (request.getParameter("login")=="moi" && request.getParameter("password")=="1234"){
-            jspcalling("page.html", request, response);
+        System.out.println("dopost");
+        if (request.getParameter("login").equals("moi") && request.getParameter("password").equals("1234")){
+            jspcalling("page.jsp", request, response);
         }
     }
 
---
+++ web/page.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : page
+    Created on : 24 févr. 2016, 17:05:50
+    Author     : FT
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="fr-fr">
+	<head>
+		<title>test HTML</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+            <p>coucou</p>
+	</body>
+</html>
